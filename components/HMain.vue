17d99e5a458657aa2c2ae216c9a65e1fd795f270
akbarSalahshour <akbarsalahshour3230@gmail.com>
2020-12-31 17:52:14 +0330

    create header

---
+++ components/HMain.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <b-navbar toggleable="md" type="dark" variant="dark">
+      <b-container>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <nuxt-link to="/" class="text-white d-inline-block py-2"
+              >صفحه اصلی</nuxt-link
+            >
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h-main />
     <Nuxt />
   </div>
 </template>
